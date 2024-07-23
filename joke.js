cdf98d93e7656b264a89d9e89b751281a2025166
MADHAN K <73920531+MADHAN79@users.noreply.github.com>
2024-07-23 19:43:27 +0530

    Add files via upload

---
+++ joke.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function()  {
+    
+    const generateBtn = document.getElementById("generateBtn");
+
+function fetchJoke() {
+    fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('joke').innerText = data.joke;
+    })
+    .catch(error => console.error('Error fetching joke:', error));
+}
+
+function runConfetti() {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+}
+
+generateBtn.addEventListener("click", function() {
+    fetchJoke();
+    runConfetti();
+});
+
+fetchJoke();
+});
